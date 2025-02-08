f382fbf27471509f979e2c25440112c2fff3b9e2
Umair Fuward <usamafuward2001@gmail.com>
2025-02-08 20:11:51 +0530

    Portfolio changed

--- src/components/ui/AnimatedText.jsx
+++ src/components/ui/AnimatedText.jsx
@@ -1,6 +1,9 @@
 /* eslint-disable react/prop-types */
+import { DarkModeContext } from "@/context/DarkModeContext";
+import { useContext } from "react";
 
 const AnimatedText = ({ texts = ["AI/ML Engineer", "Software Developer"] }) => {
+  const { darkMode } = useContext(DarkModeContext);
   return (
     <div className="relative h-[38px] overflow-hidden">
       {texts.map((text, index) => (
@@ -9,7 +12,7 @@ const AnimatedText = ({ texts = ["AI/ML Engineer", "Software Developer"] }) => {
           className="absolute w-full text-2xl md:text-3xl font-medium"
           style={{
             color: "transparent",
-            WebkitTextStroke: "0.7px #2dd4bf",
+            WebkitTextStroke: darkMode ? "#2DD4BF" : "#0d9488",
             animation: `displayText 8s linear infinite`,
             animationDelay: `calc(-4s * ${index})`,
           }}
@@ -17,9 +20,9 @@ const AnimatedText = ({ texts = ["AI/ML Engineer", "Software Developer"] }) => {
           <span className="relative inline-block">
             {text}
             <span
-              className="absolute left-0 top-0 overflow-hidden whitespace-nowrap border-r-2 border-teal-400"
+              className="absolute left-0 top-0 overflow-hidden whitespace-nowrap border-r-2 dark:border-teal-400 border-teal-600"
               style={{
-                color: "#2dd4bf",
+                color: darkMode ? "#2DD4BF" : "#0d9488",
                 width: 0,
                 animation: "fillText 4s linear infinite",
                 animationDelay: `calc(-4s * ${index})`,
